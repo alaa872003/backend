1f72dbe93fcacfc640246565446e10249ebc16ee
alaa872003 <alaa.moh.latif@gmail.com>
2024-09-11 01:18:45 +0300

    add task6-9

---
+++ Task(6-9)/P01_SalesDatabase/P01_SalesDatabase/Data/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_SalesDatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01_SalesDatabase.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=LENOVO;Initial Catalog=Sales;Integrated Security=True;TrustServerCertificate=True");
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>().Property(p=>p.Name).HasColumnType("varchar(50)").IsUnicode(true);
+            modelBuilder.Entity<Customer>().Property(c => c.Name).HasColumnType("varchar(100)").IsUnicode(true);
+            modelBuilder.Entity<Customer>().Property(c => c.Email).HasColumnType("varchar(80)").IsUnicode(false);
+            modelBuilder.Entity<Store>().Property(s => s.Name).HasColumnType("varchar(80)").IsUnicode(true);
+
+            
+
+        }
+    }
+}
---
+++ Task(6-9)/P01_SalesDatabase/P01_SalesDatabase/Models/Sale.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01_SalesDatabase.Models
+{
+    internal class Sale
+    {
+        public int Id { get; set; }
+        public Product product { get; set; }
+        public Customer customer { get; set; }
+        public Store store { get; set; }
+        public int ProductId { get; set; }
+        public int CustomerId { get; set; }
+        public int StoreId { get; set; }
+
+        public DateTime Date { get; set; }
+
+
+    }
+}
